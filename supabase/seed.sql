2fc87a6df8c1cebae4065d9aae3ffe3bc4651ba4
Soren <avantwebdev@protonmail.com>
2023-03-22 16:00:35 +0200

    commit all

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,7 +1,23 @@
- INSERT INTO note (title,content) VALUES ('first note', 'some note description here'), ('second', 'more description'), ('remember', 'to water the plants');
+ INSERT INTO note (title, content) VALUES ('first note','some description here');
+ INSERT INTO note (title, content) VALUES ('second','... description here');
+ INSERT INTO note (title, content) VALUES ('Remember','To water the plants'); 
 
---  INSERT INTO app (name) VALUES ('Wikilection');
---  INSERT INTO app (name) VALUES ('Corporate');
+ INSERT INTO app (url_name, logo_url) VALUES ('Wikilection', 'logo.png');
+ INSERT INTO app (url_name, logo_url) VALUES ('Corporate', 'logo.png');
+
+
+ INSERT INTO page_module (url_name, title, app) VALUES ('people','People', 'Wikilection');
+ INSERT INTO page_module (url_name, title, app) VALUES ('governments','Governments', 'Wikilection');
+ INSERT INTO page_module (url_name, title, app) VALUES ('news','News', 'Wikilection'); 
+
+
+ INSERT INTO page_module (url_name, title, app) VALUES ('people','People', 'Corporate');
+ INSERT INTO page_module (url_name, title, app) VALUES ('organizations','Organizations', 'Corporate');
+ INSERT INTO page_module (url_name, title, app) VALUES ('news','News', 'Corporate'); 
+--  INSERT INTO () VALUES ();
+--  INSERT INTO () VALUES ();
+--  INSERT INTO () VALUES ();
+
 
 --  INSERT INTO moduletype (name, renderfunction, data_jsonschema) VALUES ('Home', 'just testing', 'just testing');
 --  INSERT INTO moduletype (name, renderfunction, data_jsonschema) VALUES ('People', 'just testing', 'just testing');
