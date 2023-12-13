1dfe5c19575e92709562ab2da9acbde79a779d8d
Soren-365 <avantwebdev@protonmail.com>
2023-12-13 12:09:42 +0200

    redirect root path '/' to login

--- app-client/app/page.tsx
+++ app-client/app/page.tsx
@@ -2,9 +2,48 @@
 
 
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation'
 
+interface AppPageProps {
+  params: {appName: string}
+}
+
+
+// const getAppData = async (appName: AppPageProps["params"]["appName"]) => {
+    
+//   // console.log('appName', appName);
+
+//   const { data, error } = await supabase
+//     .from('app')
+//     .select(`url_name, id, logo_url`)
+//     .eq('url_name', appName);
+//   if (error) {
+//     console.log('Error', error);  
+//     throw new Error(`Error from db with app name: ${appName}`);  
+//   }
+//   if (!data) {
+//     throw new Error(`ERROR: No Data in DB for this app name: ${appName}`);
+//   }
+
+//   //  console.log('data', JSON.stringify(data, null, 2));
+//   return data
+  
+// };
 
 export default function HomePage() {
+
+   redirect('/login')
+
+  //   const router = useRouter()
+  // if (appData) {
+  //   console.log('appData !!', appData);
+  //   if (appData.default_url_module_name !== null && appName) {
+  //     router.push(`/${appName}/${appData.default_url_module_name}`)
+  //   }
+  // }
+
+
   return (
     <div className="py-10">
     <header>
